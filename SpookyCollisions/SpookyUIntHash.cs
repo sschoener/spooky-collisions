b58800d942523f2ebcc57fc5f3bd83a8b1a1c80b
Sebastian Schöner <sebastian.schoener@gmail.com>
2019-09-05 17:33:37 +0200

    Add a bucketing scheme to detect collisions
    This can verify that there are in fact no collisions within 20 minutes.

--- SpookyCollisions/SpookyUIntHash.cs
+++ SpookyCollisions/SpookyUIntHash.cs
@@ -1,4 +1,5 @@
-﻿using SpookilySharp;
+﻿using System.Runtime.CompilerServices;
+using SpookilySharp;
 
 namespace SpookyCollisions
 {
@@ -26,10 +27,6 @@ namespace SpookyCollisions
             return (x, y);
         }
 
-        public static void CleanHash(uint n, ref ulong hash1, ref ulong hash2) {
-
-        }
-
         public static void Hash(uint n, ref ulong hash1, ref ulong hash2) {
             ulong a0 = hash1;
             ulong b0 = hash2;
@@ -50,6 +47,42 @@ namespace SpookyCollisions
 
             hash2 = b5;
             hash1 = a6;
+        }
+
+        /// <summary>
+        /// This hash function is slightly quicker than the one above because we skip the last
+        /// operation. Scramble is invertible and we'll only get a collision in the last step
+        /// if the values were already colliding before.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="hash1"></param>
+        /// <param name="hash2"></param>
+        public static void QuickHash(uint n, ref ulong hash1, ref ulong hash2) {
+            ulong a0 = hash1;
+            ulong b0 = hash2;
+            ulong c0 = SpookyConst + n;
+            ulong d0 = SpookyConst + (4ul << 56);
+
+            var (d1, c1) = Scramble(d0, c0, 15);
+            var (a1, d2) = Scramble(a0, d1, 52);
+            var (b1, a2) = Scramble(b0, a1, 26);
+            var (c2, b2) = Scramble(c1, b1, 51);
+            var (d3, c3) = Scramble(d2, c2, 28);
+            var (a3, d4) = Scramble(a2, d3, 9);
+            var (b3, a4) = Scramble(b2, a3, 47);
+            var (c4, b4) = Scramble(c3, b3, 54);
+            var (d5, c5) = Scramble(d4, c4, 32);
+            var (a5, d6) = Scramble(a4, d5, 25);
+
+            hash2 = b4;
+            hash1 = a5;
+        }
+
+        public static HashCode128 QuickHash(uint n) {
+            ulong hash1 = SpookyConst;
+            ulong hash2 = SpookyConst;
+            QuickHash(n, ref hash1, ref hash2);
+            return new HashCode128(hash1, hash2);
         }
 
         public static HashCode128 Hash(uint n) {
--- SpookyExperiments/Program.cs
+++ SpookyExperiments/Program.cs
@@ -1,23 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SpookilySharp;
 
 namespace SpookyCollisions
 {
     class Program
-    {
-        const ulong SpookyConst = 0xDEADBEEFDEADBEEF;
+    {      
+        const int BucketBits = 8;
+        const ulong ExpectedBucketSize = 0x1ul << (32 - BucketBits);
+        const ulong NumBuckets = 0x1ul << (BucketBits);
+        const ulong BucketMask = ~0x0ul >> (64 - BucketBits);
+        const ulong NumHashesComputed = NumBuckets << 32;
+
+        private static IEnumerable<(uint,uint)> FindCollisions(ulong bucket)
+        {
+            var elements = new List<(HashCode128, uint)>((int)ExpectedBucketSize);
+            for (ulong v = 0; v <= uint.MaxValue; v++) {
+                uint n = (uint)v;
+                var hashCode = SpookyUIntHash.QuickHash(n);
+                if ((hashCode.UHash1 & BucketMask) == bucket) {
+                    elements.Add((hashCode, n));
+                }
+            }
+
+            var hashes =  new Dictionary<HashCode128, uint>((int)ExpectedBucketSize);
+            foreach (var tpl in elements) {
+                if (!hashes.TryAdd(tpl.Item1, tpl.Item2)) {
+                    yield return (tpl.Item2, hashes[tpl.Item1]);
+                }
+            }
+        }
 
         static void Main(string[] args)
         {
+            Console.WriteLine($"Bucket bits: {BucketBits}, num buckets: {NumBuckets}");
+            float bucketSizeMB = 4 * ExpectedBucketSize / (float) (0x1u << 20);
+            Console.WriteLine($"Expected bucket size: {ExpectedBucketSize} elements, {bucketSizeMB}mb");
+
+            Console.WriteLine("Collisions: ");
+
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            uint even = 0;
-            for (ulong c = 0; c <= uint.MaxValue; c++) {
-                var result = SpookyUIntHash.Hash((uint)c);
-                even += (uint)(result.UHash2 & 0x1);
+            foreach (var tuple in Enumerable.Range(0, (int) NumBuckets).AsParallel()
+                                    .SelectMany(b => FindCollisions((ulong)b)) {
+                Console.WriteLine(tuple);
             }
             watch.Stop();
-
-            Console.WriteLine($"Result: {even} evens (of {uint.MaxValue}), that's {100 * (even/(double)uint.MaxValue)}%");
-            Console.WriteLine($"Took {watch.ElapsedMilliseconds}ms\n{1000 * (uint.MaxValue / ((double)watch.ElapsedMilliseconds))} H/s");
+            
+            Console.WriteLine($"Took {watch.ElapsedMilliseconds}ms");
+            double hashesPerSecond = NumHashesComputed / ((double)watch.ElapsedMilliseconds * 1E-3);
+            Console.WriteLine($"About {hashesPerSecond} H/s");
         }
     }
 }
