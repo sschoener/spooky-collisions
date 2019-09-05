99269ab40a09d1f3afa2cb884f6044ecafa9cb58
Sebastian Schöner <sebastian.schoener@gmail.com>
2019-09-05 07:32:26 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace spooky_collisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
